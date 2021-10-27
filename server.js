a07de1d4692273d35f384336c4d010fa016274f3
HariharasudhanMathi <havocshari@gmail.com>
2021-10-27 12:07:36 +0530

    initial commit

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.post('/getweather', (req, res) => {
 
 
 
-
-app.listen("5000", (err) => {
+let port=process.env.PORT || 5000;
+app.listen(port, (err) => {
     if(err)conaole.log(err);
     console.log("Server is Up on 5000");
 })
